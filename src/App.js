1878780d1e51d2bebe7cb2a64b3d5de28811fa1b
Steven  Yoh <stevenyoh@stevens-mbp.lan>
2020-08-12 15:28:03 -0700

    Implemented UI space theme. Nav bar has dropdown planets for each tab.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import configureStore from './templates/config/configureStore';
-import { Provider } from 'react-redux';
+
 import axios from 'axios';
-import Menu from './components/Menu.js';
+// import Menu from './components/Menu.js';
 import Home from './components/Home.js';
 import About from './components/About.js';
 import Portfolio from './components/Portfolio.js';
-import Work from './components/Work.js';
 import Contact from './components/Contact.js';
 import TwentyFourtyEight from './components/TwentyFourtyEight.js';
 import SimpleModal from './components/SimpleModal.js';
@@ -19,30 +17,13 @@ import Loader from './components/Loader.js';
 import './css/App.css';
 import $ from "jquery";
 
-const store = configureStore();
-class DynamicImport extends Component {
-  state = {
-    component: null
-  }
-  componentWillMount() {
-    this.props.load()
-    .then((mod) => {
-      return mod;
-    })
-      .then((mod) => this.setState(() => ({
-        component: mod.default
-      })))
-  }
-  render() {
-    return this.props.children(this.state.component)
-  }
-}
 
 class App extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
       loading: false,
+      tabs: ["home", "about", "resume", "portfolio", "contact"]
     }
     this.loader = this.loader.bind(this);
   }
@@ -55,48 +36,83 @@ class App extends React.Component{
       )
     }
     if(!loading) {
+      
       return(
         <div className="app-wrapper">
-          <Swing />
-          {/* <Home /> */}
-          <Playground />
-          {/* <div className="pimg1">
-            <div className="ptext">
-              <Home />
-            </div>
+          <div className="menu">
+            <span/>
+            <a id="menu-home" href="/">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              {"Home"}
+            </a>
+            <span/>
+            <span/>
+            <a id="menu-about"href="about">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              {"About"}
+            </a>
+            <span/>
+            <span/>
+            <a id="menu-portfolio" href="portfolio">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              {"Portfolio"}
+            </a>
+            <span/>
+            <span/>
+            <a id="menu-contact" href="contact">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              {"Contact"}
+            </a>
+            <span/>
           </div>
-          <section className="section section-light">
-            <Swing />
-          </section>
-    
-          <div className="pimg2">
-            <div className="ptext">
-            <span className="border">
-            </span>
-            </div>
-          </div>
-          <section className="section section-light">
-      
-          </section>
-  
-          <div className="pimg3">
-            <div className="ptext">
-            <span className="border">
-              
-            </span>
-            </div>
-          </div>
-          <section className="section section-dark">
-            <div className="twentyFourtyEightBtn">
-              <SimpleModal className="simpleModal"/>
-            </div>
-          </section>  */}
+          
+          <Router>
+            <Route path="/" component={Home}></Route>
+            <Route path="/about" component={About}></Route>
+            <Route path="/portfolio" component={Portfolio}></Route>
+            <Route path="/contact" component={Contact}></Route>
+          </Router>
         </div>    
       )
     }
   }
   componentDidMount() {
-    setTimeout(() => (this.setState({loading: false})),7000)
+    // setTimeout(() => (this.setState({loading: false})),7000)
+    var menu = ["menu-home", "menu-about", "menu-portfolio", "menu-contact"];
+    var planets = ["sun", "earth", "triple", "saturn"];
+    menu.map((tab, index) => {
+      var element = document.getElementById(tab);
+      var planet = document.getElementById(planets[index])
+      var preview = `${planet.id}-preview`
+      element.onmouseover = () => {
+        planet.id = preview
+      }
+      element.onmouseleave = () => {
+        planet.id = planets[index];
+      }
+    })
+    // planets.map((planet) => {
+    //   var element = document.getElementById(planet)
+    //   element.onclick = () => {
+    //     element.id = `${planet}-active`;
+    //   }
+    // })
   }
   render() {
       return (
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,7 +13,13 @@ const Contact = () => {
   }
   return (
     <div className="contact">
-      <address className="text">
+      <svg width="1440" height="900" viewBox="0 0 1440 900" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g id="blob">
+      <path id="blob-background" d="M702.884 101.574C1034.17 83.4912 1302.84 203.364 1319.07 368.158C1340.24 583.062 1114.75 877.749 735.56 700.224C443.367 563.427 128.394 598.953 119.371 433.64C110.348 268.327 371.595 119.656 702.884 101.574Z" fill="#F2C38B" fill-opacity="0.8"/>
+      </g>
+      </svg>
+
+      {/* <address className="text">
         {contact.email}<br/>
         {contact.location}<br/>
         {contact.phone}
@@ -44,7 +50,7 @@ const Contact = () => {
       </a><br/>
       <a href={contact.github.href}>
         <img  className="github" src={contact.github.img} alt=""/>
-      </a><br/>
+      </a><br/> */}
     </div>
   )
 }
